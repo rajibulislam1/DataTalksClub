d10900a72243335633e31143e7e13178931a1f29
Rajibul <rajibul005@gmail.com>
2025-11-18 22:01:56 +0100

    Web serving

--- 2025/Midterm/ping.py
+++ 2025/Midterm/ping.py
@@ -7,7 +7,3 @@ def ping_route():
     return ping()  # calls the function we defined
 
 app.run(debug=True, host = '0.0.0.0')
-
-
-def ping():
-    retun 'PONG'
--- 2025/Midterm/test_predict.py
+++ 2025/Midterm/test_predict.py
@@ -1,11 +1,1 @@
-input_data = {'parents': 'great_pret', 
-              'has_nurs':'critical', 
-              'form':'complete', 
-              'children':3, 
-              'housing':'convenient', 
-              'finance':'convenient',
-              'social':'nonprob', 
-              'health':'priority'}
 
-print("Predicted class:", pred_class)
-print("Prediction probabilities:", pred_proba)
