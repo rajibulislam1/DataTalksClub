1fcf4f900c4406d8efba96de16fe60c02d723ece
Rajibul <rajibul005@gmail.com>
2025-11-23 00:02:31 +0100

    Train and Test data for model flask api

--- 2025/Midterm/predict.py
+++ 2025/Midterm/predict.py
@@ -2,5 +2,28 @@
 import pickle
 output = 'logisticmodel.bin'
 
-with open(output, 'wb') as f_out:
-    pickle.dump((dv, log), f_out)
+with open(output, 'rb') as f_in:
+    dv, log = pickle.load(f_in)
+
+
+
+
+new_data = {
+    'parents':'usual', 
+    'has_nurs':'proper', 
+    'form':'completed',
+    'children':3,
+    'housing':'convenient', 
+    'finance':'convenient',
+    'social':'slightly_prob', 
+    'health':'priority'
+}
+
+
+X = dv.transform([new_data])
+
+
+y_pred = log.predict_proba(X)[0, 1]
+
+print('input', new_data)
+print('Class recommended',y_pred)
