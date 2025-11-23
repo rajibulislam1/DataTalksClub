a1247bde880e250d1b6e39e103367f50dd2341e1
Rajibul <rajibul005@gmail.com>
2025-11-23 14:40:04 +0100

    Gunicorn server with Flask

--- 2025/Midterm/predict.py
+++ 2025/Midterm/predict.py
@@ -1,29 +1,32 @@
 # Load The model
 import pickle
+from flask import Flask, request, jsonify
+
 output = 'logisticmodel.bin'
 
 with open(output, 'rb') as f_in:
     dv, log = pickle.load(f_in)
 
+app = Flask('Admission_Result')
 
+@app.route("/predict", methods=['POST'])
+def predict():
+    applicants_info = request.get_json()
 
+    X = dv.transform([applicants_info])
+    y_pred = log.predict_proba(X)[0, 1]
 
-new_data = {
-    'parents':'usual', 
-    'has_nurs':'proper', 
-    'form':'completed',
-    'children':3,
-    'housing':'convenient', 
-    'finance':'convenient',
-    'social':'slightly_prob', 
-    'health':'priority'
-}
+    if y_pred >= 0.8:
+        admission = True
+    else:
+        admission = False
 
+    result = {
+        'Admission_Class': float(y_pred),
+        'Admission_Recommend': admission
+    }
 
-X = dv.transform([new_data])
+    return jsonify(result)
 
-
-y_pred = log.predict_proba(X)[0, 1]
-
-print('input', new_data)
-print('Class recommended',y_pred)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9696)
